a5917395a49e4b7f81ffc238f069b3d0cf5b164c
xahram <ssasuke1163@gmail.com>
2020-06-12 16:26:03 +0500

    Index html change

--- front/src/components/Dashboard/RightSection/RightSection.js
+++ front/src/components/Dashboard/RightSection/RightSection.js
@@ -3,6 +3,7 @@ import classes from './RightSection.module.css'
 import Chart from '../../Chart'
 const rightSection = (props) => {
     return (<div className={classes.rightSection}>
+        <h3>Data Usage</h3>
             <Chart />
     </div>)
 }
